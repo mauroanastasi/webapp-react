0a785e0d86f29d58c5c9374505ed86c9dad035b3
mauroanastasi <mauroanastasi1@gmail.com>
2025-06-02 14:18:10 +0200

    [UPD]MIL_2.OK creo pagina di dettaglio film

--- pages/MoviePage.jsx
+++ pages/MoviePage.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 const MoviePage = () => {
     return (
         <div className='row' >
-            <div className='col-12 col-md-6 col-lg-4'>Img</div>
+            <div className='col-12 col-md-6 col-lg-4'>
+                <img src="https://picsum.photos/300/300" className='img-fluid' alt="movie" />
+            </div>
             <div className='col-12 col-md-6 col-lg-8'>
                 <h1>Titolo</h1>
-                <p>Altre Informazioni</p>
+                <h1>Regista</h1>
+                <h1>Attori</h1>
+                <p>Descrizione</p>
             </div>
         </div>
     );
