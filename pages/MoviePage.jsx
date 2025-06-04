ba64f11f9fc954437967c2579a33067d3e5595ae
mauroanastasi <mauroanastasi1@gmail.com>
2025-06-04 17:15:57 +0200

    [UPD] faccio chiamata ajax all'interno di moviePage

--- pages/MoviePage.jsx
+++ pages/MoviePage.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const MoviePage = () => {
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+    const fetchMovies = () => {
+
+        axios.get(`http://127.0.0.1:3000/api/movies/${id}`).then((resp) => {
+            setMovie(resp.data);
+        })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    useEffect(fetchMovies, []);
     return (
-        <div className='row' >
-            <div className='col-12 col-md-6 col-lg-4'>
-                <img src="https://picsum.photos/300/300" className='img-fluid' alt="movie" />
+        <>
+            <div className='row'>
+                {movie === null ? (
+                    `caricamento dati libro`
+                ) : (
+                    <>
+                        <div className='col-12 col-md-6 col-lg-4'>
+                            <img src={movie.image} className='img-fluid' alt="movie" />
+                        </div>
+                        <div className='col-12 col-md-6 col-lg-8'>
+                            <h1>{movie.title}</h1>
+                            <h1>{movie.director}</h1>
+                            <h1>{movie.genre}</h1>
+                            <p>{movie.abstract}</p>
+                        </div>
+                    </>
+                )}
             </div>
-            <div className='col-12 col-md-6 col-lg-8'>
-                <h1>Titolo</h1>
-                <h1>Regista</h1>
-                <h1>Attori</h1>
-                <p>Descrizione</p>
-            </div>
-        </div>
+        </>
     );
 };
 
